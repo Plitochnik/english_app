971cd8842e921023efa5539790d8f0016a80a199
plitochnik <vitalikplitochnik@gmail.com>
2022-12-06 22:43:27 +0200

    has been set up laravel jetstream

--- resources/js/Layouts/HeaderLayout.vue
+++ resources/js/Layouts/HeaderLayout.vue
@@ -1,7 +1,7 @@
 <template>
 
     <body class="u-body u-xl-mode" data-lang="ru">
-    <header class="u-custom-color-1 u-header" id="sec-0683" data-animation-name="" data-animation-duration="0"
+    <header class="flex items-center justify-center u-custom-color-1 u-header" id="sec-0683" data-animation-name="" data-animation-duration="0"
             data-animation-delay="0" data-animation-direction="">
         <div class="u-align-left u-clearfix u-sheet u-sheet-1">
             <Link :href="route('index.parameters')"
@@ -9,7 +9,7 @@
                 Start test
             </Link>
             <Link :href="route('index.signin')"
-                  class="font u-border-2 u-border-hover-palette-1-base u-border-palette-1-base u-btn u-btn-round u-button-style u-hover-palette-1-base u-none u-radius-50 u-btn-2">
+                  class="ml-3 font u-border-2 u-border-hover-palette-1-base u-border-palette-1-base u-btn u-btn-round u-button-style u-hover-palette-1-base u-none u-radius-50 u-btn-2">
                 Sign in
             </Link>
         </div>
@@ -20,7 +20,6 @@
 
 <script>
 import {Link} from "@inertiajs/inertia-vue3";
-import "../../../public/nicepage_layout/css/nicepage.css";
 
 
 export default {
---
+++ resources/js/Pages/Dashboard.vue
@@ -0,0 +1,34 @@
+<template>
+    <h1 class="text-xl">
+        Dashboard Page
+    </h1>
+    <AppLayout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                Dashboard
+            </h2>
+        </template>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                    <Welcome/>
+                </div>
+            </div>
+        </div>
+    </AppLayout>
+</template>
+
+<script>
+
+import AppLayout from '../Layouts/AppLayout.vue';
+import Welcome from '../Components/Welcome.vue';
+
+export default {
+    components: {
+        AppLayout,
+        Welcome,
+    },
+}
+
+</script>
