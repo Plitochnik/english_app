09ae892ad29d9678b0cd18e8cf4fee97ed420454
vital <vitalikplitochnik@gmail.com>
2023-03-14 16:32:31 +0000

    finished test animation fixed

--- app/Http/Controllers/PushValues/PushToDatabaseController.php
+++ app/Http/Controllers/PushValues/PushToDatabaseController.php
@@ -10,6 +10,8 @@ use Illuminate\Support\Facades\Storage;
 class PushToDatabaseController extends Controller
 {
     // код по пушу англ слов ниже
+    public array $english_words, $ukrainian_words, $russian_words, $spanish_words;
+
     public function storageFilesToArrayAndDeleteTrims($filename): array
     {
         $content = Storage::disk('local')->get($filename);
@@ -22,5 +24,25 @@ class PushToDatabaseController extends Controller
         return $words;
     }
 
+    public function __construct()
+    {
+        $this->english_words = $this->storageFilesToArrayAndDeleteTrims('english_words.txt');
+        $this->ukrainian_words = $this->storageFilesToArrayAndDeleteTrims('ukrainian_words.txt');
+        $this->russian_words = $this->storageFilesToArrayAndDeleteTrims('russian_words.txt');
+        $this->spanish_words = $this->storageFilesToArrayAndDeleteTrims('spanish_words.txt');
+    }
+
+    public function getAndPushValuesToDatabase($english_words,$ukrainian_words, $russian_words, $spanish_words): void
+    {
+        for ($i = 0; $i <= count($english_words); $i++) {
+
+            Words::create([
+                'english' => ucfirst($english_words[$i]),
+                'ukrainian' => ucfirst($ukrainian_words[$i]),
+                'russian' => ucfirst($russian_words[$i]),
+                'spanish' => ucfirst($spanish_words[$i]),
+            ]);
+        }
+    }
 
 }
